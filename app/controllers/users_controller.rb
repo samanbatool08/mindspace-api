1269c2ef12faff8d5f869c4abb060aae13dcad79
Saman Batool <samanbatool08@gmail.com>
2023-02-19 17:20:27 -0500

    user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all 
-    render json: @users
+    users = User.all 
+    render json: users
+  end
+
+
+  def create
+    user = User.new(user_params)
+    if user.save 
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :email)
   end
 
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates_presence_of :username, :email
+  validates_length_of :username, in: 5..30
 end
